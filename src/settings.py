4279ee7e410bc60ee97c3791413da8d10b22f7c3
tweek36 <sowakov.yura@gmail.com>
2026-06-24 22:17:18 +0300

    init

--- src/settings.py
+++ src/settings.py
@@ -4,10 +4,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    database_url: str = (
-        "postgresql+asyncpg://postgres:postgres@localhost:5435/search_db"
-    )
+    postgres_host: str
+    postgres_database_name: str
+    postgres_password: str
+    postgres_port: int
+    postgres_username: str
+
     kafka_bootstrap_servers: str = "localhost:9092"
     kafka_topic_ads: str = "ads"
     kafka_consumer_group: str = "search-service"
     ad_service_url: str = "http://localhost:8002"
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.postgres_username}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_database_name}"
